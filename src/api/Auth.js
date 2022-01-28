7657e5c5ef145e0ee65953be2d2ebf9a06936d98
SaidAoussar <aoussarsaid33@gmail.com>
2022-01-28 18:42:27 +0100

    create Auth.js for hadling fetch api for my  api

---
+++ src/api/Auth.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const URL = process.env.REACT_APP_URL
+
+export const login = async (data) => {
+  try {
+    const doc = await axios.post(`${URL}/login`,data)
+    return doc
+  } catch (e) {
+    console.log(e)
+  }
+  
+}
+
+//get user info
+export const isAuthenticated = async () =>{
+  const token = localStorage.getItem("token")
+  const doc = await axios.get(`${URL}/isAuthenticated`,{
+    headers:{
+      "auth-token": token
+    }
+  })
+  return doc
+  
+
+}
+
+
+
+
