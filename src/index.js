7d180223526ba54098291f11b5cd22a08bbddf1a
SaidAoussar <aoussarsaid33@gmail.com>
2022-01-28 18:38:09 +0100

    setup router-dom and global css in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom'
+import Routers from './router/Routers'
+import Home from './Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/global.css';
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Home></Home>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
