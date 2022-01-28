329928035f9536ed5bbc2a657e81c5625603ee30
SaidAoussar <aoussarsaid33@gmail.com>
2022-01-28 18:37:20 +0100

    add check isAuthenticated fun in Home

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import NavBar from './components/NavBar/navbar'
+import React,{useEffect, useState} from 'react'
+import { Navbar } from 'react-bootstrap';
+import NavBar from './components/NavBar/NavBar';
+import {isAuthenticated} from './api/Auth'
 
 export default function Home() {
+
+  const [user,setUser] = useState({})
+  const [isAuth,setIsAuth] = useState()
+  useEffect(()=>{
+      isAuthenticated().then((res)=>{
+        setUser(res.data)
+        setIsAuth(res.data.isAuth)
+        console.log(res.data)
+      })
+      .catch(e => {
+        setIsAuth(false)
+        setUser({})
+        console.log(e.response.data.message)
+    });
+
+  },[])
     return (
         <div>
-			<NavBar />
+          <NavBar useAuth={[isAuth,setIsAuth]} user={user}></NavBar>
         </div>
     )
 }
