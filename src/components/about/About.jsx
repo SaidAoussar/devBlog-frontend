1093431d64811cdbbcfe9b9dd5c310096291dd40
SaidAoussar <aoussarsaid33@gmail.com>
2022-01-28 18:38:33 +0100

    add about page

---
+++ src/components/about/About.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import {isAuthenticated} from '../../api/Auth'
+
+
+function About() {
+  const [user,setUser] = useState({})
+  const [error,setError] = useState("")
+  const getInfoUser = ()=>{
+    isAuthenticated().then((res)=>{
+      setError("")
+      setUser(res.data)
+      console.log(res.data)
+    })
+    .catch(e => {
+      setUser({})
+      console.log(e.response.data.message)
+      setError(e.response.data.message)
+  });
+  }
+  return <div>
+    <div>hello from about us</div>
+    <Button onClick={getInfoUser}>get auth</Button>
+    {
+      user && <div>{user.username}</div>
+    }
+    {
+      error && <div>{error}</div>
+    }
+    </div>;
+}
+
+export default About;
