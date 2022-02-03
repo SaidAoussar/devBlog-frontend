cc2d703e4b8c7fa57e7aaa147eef6c32b7e882f5
SaidAoussar <aoussarsaid33@gmail.com>
2022-02-03 18:23:35 +0100

    add two link get all blog of user and create blog

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from 'react'
+import {useParams, NavLink, Routes, Route} from 'react-router-dom'
+import {getUser} from '../../api/User'
 import { Container, Row, Col, Card, Image } from 'react-bootstrap'
-import {isAuthenticated} from '../../api/Auth'
+import UserBlogs from './UserBlogs'
+import CreateBlog from './CreateBlog'
 import avatar from '../../assets/img/avatar.png'
 import './profile.module.css';
 
+
 function Profile() {
+
+  const { id } = useParams()
   const [user,setUser] = useState({})
 
   useEffect(()=>{
-    isAuthenticated().then((res)=>{
+    getUser(id).then((res)=>{
       setUser(res.data)
     }).catch((e)=>{
       console.log(e)
     })
   },[])
-  return <Container>
+  return <Container className='mb-5'>
     <Row>
       <Col>
         <Card>
@@ -25,6 +31,21 @@ function Profile() {
           </Card.Body>    
         </Card>
       </Col>
+        
+    </Row>
+    <Row>
+      <Col><NavLink to="blogs" className={({isActive})=>{
+        return "btn d-block btn-" + (isActive ? "primary" : "secondary")
+      }}>All Bogs</NavLink></Col>
+      <Col><NavLink to="create" className={({isActive})=>{
+        return "btn d-block btn-" + (isActive ? "primary" : "secondary")
+      }}>create Bogs</NavLink></Col>
+    </Row>
+    <Row className="mt-4 mb-5 justify-content-md-center">
+      <Routes>
+          <Route path="/blogs" element={<UserBlogs userId={id}/>}/>
+          <Route path="/create" element={<CreateBlog/>}/>
+      </Routes>
     </Row>
   </Container>;
 }
