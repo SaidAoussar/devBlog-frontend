b2a6e57db44d1cf7cb80f13774d12c4591f9ed2d
SaidAoussar <aoussarsaid33@gmail.com>
2022-02-03 18:24:35 +0100

    create file for store tags

---
+++ src/components/utils/tagsData.js
@@ -0,0 +1,13 @@
+
+
+const tags = [
+  { value: 'Html', label: 'Html' },
+  { value: 'Css', label: 'Javascript' },
+  { value: 'Javascript', label: 'Javascript' },
+  { value: 'ReactJs', label: 'ReactJs' },
+  { value: 'NodeJs', label: 'NodeJs' },
+  { value: 'MongoDb', label: 'MongoDb' }
+]
+
+
+module.exports.tags = tags
