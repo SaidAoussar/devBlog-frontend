681cb58d5ae56a79a1982a64276e7a9e8678d730
SaidAoussar <aoussarsaid33@gmail.com>
2022-02-03 18:23:54 +0100

    show all blog of user

---
+++ src/components/profile/UserBlogs.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import { Row,Col } from "react-bootstrap"
+import BlogCard from '../utils/BlogCard'
+import {allBlogsOfUser} from '../../api/Blog'
+
+function UserBlogs({userId}) {
+  const [blogs, setBlogs] = useState([]) 
+
+  useEffect(()=>{
+    allBlogsOfUser(userId).then((res)=>{
+      setBlogs(res.data)
+    }).catch((e)=>{
+      console.log(e)
+    })
+  },[])
+  return ( 
+      <Row xs={1} sm={2} md={3} lg={4} className="g-3">
+        {
+          blogs.map((blog)=>{
+            return <Col key={blog._id}>
+              <BlogCard blog={blog} />
+            </Col>
+          })
+        }
+      </Row>
+   );
+}
+
+export default UserBlogs;
