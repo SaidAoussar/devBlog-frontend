3e0547d6cbdc88420848103a8f27676eb2642879
SaidAoussar <aoussarsaid33@gmail.com>
2022-10-17 12:16:40 +0100

    add model for edit profil

---
+++ src/components/profile/ModalEditProfile.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenSquare } from "@fortawesome/free-solid-svg-icons";
+
+function ModalEditProfile({ children }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <FontAwesomeIcon
+        icon={faPenSquare}
+        style={{
+          fontSize: "1.5rem",
+          cursor: "pointer",
+          marginRight: "4px",
+          color: "#0d6efd",
+        }}
+        onClick={handleShow}
+      />
+      <Modal show={show} onHide={handleClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{children}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default ModalEditProfile;
