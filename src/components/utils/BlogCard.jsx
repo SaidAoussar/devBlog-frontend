bca6a56b38daff7866ebd4ffa1f760c11bae1270
SaidAoussar <aoussarsaid33@gmail.com>
2022-11-29 13:40:17 +0100

    refactor cards of blogs to antD

--- src/components/utils/BlogCard.jsx
+++ src/components/utils/BlogCard.jsx
@@ -1,11 +1,21 @@
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { AppContext } from "../../context/AppContext";
-import { RemoveBlog, allBlogsOfUser } from "../../api/Blog";
-import { Card, Tooltip, OverlayTrigger, Modal, Button } from "react-bootstrap";
+import {
+  Card as OldCard,
+  Tooltip,
+  OverlayTrigger,
+  Modal,
+  Button,
+} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { toast } from "react-toastify";
+import { Card } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { AppContext } from "../../context/AppContext";
+import { RemoveBlog, allBlogsOfUser } from "../../api/Blog";
+
+const { Meta } = Card;
 
 function BlogCard({ blog, operation, userId, setBlogs }) {
   let navigate = useNavigate();
@@ -47,45 +57,28 @@ function BlogCard({ blog, operation, userId, setBlogs }) {
   };
   return (
     <>
-      <Card>
-        {operation && user._id == userId && (
-          <Card.Header>
-            <Link to={`/profile/${userId}/blogs/${blog._id}/edit`}>
-              <FontAwesomeIcon
-                icon={faPenSquare}
-                style={{
-                  fontSize: "1.5rem",
-                  cursor: "pointer",
-                  marginRight: "4px",
-                }}
-              />
-            </Link>
-            <FontAwesomeIcon
-              icon={faTrash}
-              onClick={handleShow}
-              style={{ fontSize: "1.3rem", cursor: "pointer", color: "red" }}
-            />
-          </Card.Header>
-        )}
-
-        <Card.Img
-          variant="top"
-          src="https://via.placeholder.com/100x50.png/"
-        ></Card.Img>
-        <Card.Body>
-          <Card.Title>
-            <OverlayTrigger
-              key={blog._id}
-              placement={`top`}
-              overlay={<Tooltip id={blog._id}>{blog.title}</Tooltip>}
-            >
-              <Link className="link" key={blog._id} to={`/blog/${blog._id}`}>
-                {blog.title.slice(0, 75)}...
-              </Link>
-            </OverlayTrigger>
-          </Card.Title>
-          <Card.Text>{blog.body.slice(0, 100)}...</Card.Text>
-        </Card.Body>
+      <Card
+        cover={
+          <img
+            alt="example"
+            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          />
+        }
+        actions={
+          operation && user._id == userId
+            ? [
+                <Link to={`/profile/${userId}/blogs/${blog._id}/edit`}>
+                  <EditOutlined />
+                </Link>,
+                <DeleteOutlined onClick={handleShow} />,
+              ]
+            : []
+        }
+      >
+        <Meta
+          title={`${blog.title}`}
+          description={`${blog.body.slice(0, 100)}`}
+        />
       </Card>
       <Modal
         show={show}
