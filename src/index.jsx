3a208474c70e22669ca551981c08be0abd00a699
SaidAoussar <aoussarsaid33@gmail.com>
2022-11-29 10:17:04 +0100

    install anD and update method of render in index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/global.css";
 import { ContextWrapper } from "./context/AppContext";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
     <ContextWrapper>
       <Home></Home>
     </ContextWrapper>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
