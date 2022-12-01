b4e724be5142cc4bd235ce004f69852ecd6a209c
SaidAoussar <aoussarsaid33@gmail.com>
2022-12-01 22:27:33 +0100

    refactor blog page to antD

--- src/components/homepage/Blog.jsx
+++ src/components/homepage/Blog.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getBlog } from "../../api/Blog";
 import { getUser } from "../../api/User";
-import { Container, Row, Col, Image, Badge } from "react-bootstrap";
+import Container from "../utils/Container";
+import { Layout, Row, Col, Image, Tag } from "antd";
 
 function Blog() {
   let params = useParams();
@@ -24,30 +25,25 @@ function Blog() {
 
   return (
     <div>
-      <Container>
-        <Row className="justify-content-sm-center">
-          <Col md={8} bg="secondary">
-            <Image width="100%" src="https://via.placeholder.com/600x300" />
-            <h5>{blog.title}</h5>
-            <div>{new Date(blog.postTime).toLocaleString()}</div>
-            <small style={{ fontWeight: "bold" }}>{user?.username}</small>
-            <p>{blog.body}</p>
-            <div>
-              {blog.tags?.map((tag) => {
-                const myTags = tag.split(", ");
-                return myTags.map((myTag) => (
-                  <Badge
-                    className="py-2 px-4 ml-3"
-                    style={{ marginLeft: "4px", marginBottom: "4px" }}
-                  >
-                    {myTag}
-                  </Badge>
-                ));
-              })}
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <Layout>
+        <Container>
+          <Row justify={"center"}>
+            <Col md={14}>
+              <Image width="100%" src="https://via.placeholder.com/600x300" />
+              <h5>{blog.title}</h5>
+              <div>{new Date(blog.postTime).toLocaleString()}</div>
+              <small style={{ fontWeight: "bold" }}>{user?.username}</small>
+              <p>{blog.body}</p>
+              <div>
+                {blog.tags?.map((tag) => {
+                  const myTags = tag.split(", ");
+                  return myTags.map((myTag) => <Tag color="blue">{myTag}</Tag>);
+                })}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Layout>
     </div>
   );
 }
