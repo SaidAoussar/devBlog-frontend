ef556a4fa0f0d3efadcb8e577a1ce4d9f0647a9c
SaidAoussar <aoussarsaid33@gmail.com>
2022-12-19 16:02:23 +0100

    improve design of navabar

---
+++ info.md
@@ -0,0 +1,3 @@
+https://discover.hubpages.com/
+https://alistapart.com/
+https://www.indiehackers.com/
--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import { Button, Menu } from "antd";
+import { Button, Menu, Drawer, Avatar } from "antd";
+import {
+  LogoutOutlined,
+  MenuOutlined,
+  PlusCircleOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import { useLogout } from "../hooks/useLogout";
 
 import Container from "../components/utils/Container";
 import { useUserStore } from "../store/user";
 
-let items = [
+import "./Navbar.css";
+
+let leftItems = [
   {
     label: <Link to="/">Stories</Link>,
     key: "stories",
@@ -22,8 +30,20 @@ let items = [
   },
 ];
 
+let rightItems = [
+  {
+    label: <Link to="/login">Login</Link>,
+    key: "login",
+  },
+  {
+    label: <Link to="/register">Register</Link>,
+    key: "register",
+  },
+];
+
 function NavBar({}) {
   const [authMenu, setAuthMenu] = useState([]);
+  const [openDrawer, setOpenDrawer] = useState(false);
   const navigate = useNavigate();
 
   const authUser = useUserStore((state) => state.user);
@@ -34,43 +54,106 @@ function NavBar({}) {
     if (Object.keys(authUser).length !== 0) {
       setAuthMenu([
         {
-          label: <Link to={`/profile/${authUser.id}`}>Profile</Link>,
-          key: "profile",
-        },
-        {
-          label: (
-            <Button
-              type="link"
-              onClick={() => {
-                logout();
-                navigate("/login");
-              }}
-              style={{ color: "#000000e0" }}
-            >
-              logout
-            </Button>
-          ),
-          key: "logout",
+          label: <Avatar icon={<UserOutlined />} />,
+          key: "login",
+          children: [
+            {
+              label: (
+                <Link to={`/profile/${authUser.id}`}>
+                  <UserOutlined style={{ marginRight: "8px" }} />
+                  Profile
+                </Link>
+              ),
+              key: "profile",
+            },
+            {
+              label: (
+                <Link>
+                  <PlusCircleOutlined style={{ marginRight: "8px" }} />
+                  Create Post
+                </Link>
+              ),
+              key: "create-post",
+            },
+            {
+              label: (
+                <span
+                  onClick={() => {
+                    logout();
+                    navigate("/login");
+                  }}
+                  style={{ color: "#000000e0" }}
+                >
+                  <LogoutOutlined style={{ marginRight: "8px" }} />
+                  logout
+                </span>
+              ),
+              key: "logout",
+            },
+          ],
         },
       ]);
     } else {
-      setAuthMenu([
-        {
-          label: <Link to="/login">Login</Link>,
-          key: "login",
-        },
-        {
-          label: <Link to="/register">Register</Link>,
-          key: "register",
-        },
-      ]);
+      setAuthMenu(rightItems);
     }
   }, [authUser]);
 
   return (
     <div>
       <Container>
-        <Menu mode="horizontal" items={[...items, ...authMenu]} />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <div
+            className="right-navbar"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexGrow: 2,
+            }}
+          >
+            <span className="logo" style={{ flexGrow: 1 }}>
+              DevBlog
+            </span>
+            <div className="links hide-for-mobile" style={{ flexGrow: 3 }}>
+              <Menu mode="horizontal" items={leftItems} />
+            </div>
+          </div>
+          <div
+            className="lift_navbar flex-auto-desktop"
+            style={{ minWidth: 0 }}
+          >
+            <Button
+              className="hide-for-desktop"
+              onClick={() => setOpenDrawer(true)}
+            >
+              <MenuOutlined />
+            </Button>
+            <Menu
+              mode="horizontal"
+              className="hide-for-mobile"
+              style={{ justifyContent: "end" }}
+              items={authMenu}
+            />
+          </div>
+        </div>
+        <Drawer
+          title="DevBlog"
+          placement="right"
+          onClose={() => setOpenDrawer(false)}
+          open={openDrawer}
+        >
+          <Menu mode="inline" items={leftItems} />
+          <Menu
+            mode="inline"
+            style={{ justifyContent: "end" }}
+            items={authMenu}
+          />
+        </Drawer>
       </Container>
     </div>
   );
