95624d43b95f840c675e9fac0970006dc85bb2fd
SaidAoussar <aoussarsaid33@gmail.com>
2023-01-25 23:41:55 +0100

    feat: add dark mode for navbar

---
+++ src/layouts/navbar/NavBar.jsx
@@ -0,0 +1,143 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Menu, Drawer, Avatar, Input } from "antd";
+import {
+  LogoutOutlined,
+  MenuOutlined,
+  PlusCircleOutlined,
+  SettingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { useAtom } from "jotai";
+import * as S from "./styles";
+import { useLogout } from "../../hooks/useLogout";
+import Container from "../../components/utils/Container";
+import { useUserStore } from "../../store/user";
+import { pageNumberAtom } from "../../pages/search/store/page-number";
+
+let rightItems = [
+  {
+    label: <Link to="/login">Login</Link>,
+    key: "login",
+  },
+  {
+    label: <Link to="/register">Register</Link>,
+    key: "register",
+  },
+];
+
+const { Search } = Input;
+
+function NavBar() {
+  const [authMenu, setAuthMenu] = useState([]);
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [_, setPageNumber] = useAtom(pageNumberAtom);
+  const navigate = useNavigate();
+
+  const authUser = useUserStore((state) => state.user);
+
+  const { logout } = useLogout();
+
+  useEffect(() => {
+    if (Object.keys(authUser).length !== 0) {
+      setAuthMenu([
+        {
+          label: <Avatar src={`${import.meta.env.VITE_URL}/${authUser.img}`} />,
+          key: "auth",
+          children: [
+            {
+              label: (
+                <Link to={`/profile/${authUser.id}`}>
+                  <UserOutlined style={{ marginRight: "8px" }} />
+                  Profile
+                </Link>
+              ),
+              key: "profile",
+            },
+            {
+              label: (
+                <Link to="new">
+                  <PlusCircleOutlined style={{ marginRight: "8px" }} />
+                  Create Post
+                </Link>
+              ),
+              key: "create-post",
+            },
+            {
+              label: (
+                <Link to="/settings">
+                  <SettingOutlined style={{ marginRight: "8px" }} />
+                  Settings
+                </Link>
+              ),
+              key: "Settings",
+            },
+            {
+              label: (
+                <span
+                  onClick={() => {
+                    logout();
+                    navigate("/login");
+                  }}
+                  style={{ color: "inherit" }}
+                >
+                  <LogoutOutlined style={{ marginRight: "8px" }} />
+                  logout
+                </span>
+              ),
+              key: "logout",
+            },
+          ],
+        },
+      ]);
+    } else {
+      setAuthMenu(rightItems);
+    }
+  }, [authUser]);
+
+  const onSearch = (value) => {
+    setPageNumber(1);
+    navigate(`/search?q=${value}&filters=posts`);
+  };
+
+  return (
+    <S.Navbar>
+      <Container>
+        <S.NavbarContainer>
+          <S.LeftNavbar>
+            <S.Logo to="/">DevBlog</S.Logo>
+            <S.SearchWrapper>
+              <S.Search placeholder="Search" size="large" onSearch={onSearch} />
+            </S.SearchWrapper>
+          </S.LeftNavbar>
+          <S.RightNavbar style={{ minWidth: 0 }}>
+            <S.MobileBtnWrapper onClick={() => setOpenDrawer(true)}>
+              <MenuOutlined />
+            </S.MobileBtnWrapper>
+            <S.MenuWrapper mode="horizontal" items={authMenu} />
+          </S.RightNavbar>
+        </S.NavbarContainer>
+        <S.DrawerWrapper
+          title="DevBlog"
+          placement="right"
+          onClose={() => setOpenDrawer(false)}
+          open={openDrawer}
+        >
+          <S.MobileMenuWrapper
+            mode="inline"
+            items={authMenu}
+            defaultOpenKeys={["auth"]}
+          />
+        </S.DrawerWrapper>
+      </Container>
+    </S.Navbar>
+  );
+}
+
+export default NavBar;
+
+/***
+ * navbar i want:
+ * https://github.com/thisuraseniya/Ant-Design-Navbar/
+ *
+ */
---
+++ src/layouts/navbar/styles.js
@@ -0,0 +1,153 @@
+import { Button, Drawer, Menu, Input, theme } from "antd";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { device } from "../../utils/device";
+
+const { Search: SearchAnt } = Input;
+
+export const Navbar = styled.div`
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  margin-bottom: 16px;
+  background-color: red;
+  background-color: ${(props) => props.theme.headerBg};
+  box-shadow: 0 1px 1px ${(props) => props.theme.headerShadow};
+`;
+
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const LeftNavbar = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  flex-grow: 1;
+`;
+export const Logo = styled(Link)`
+  color: ${(props) => props.theme.bodyColor};
+  padding: 10px;
+`;
+export const SearchWrapper = styled.div`
+  width: 300px;
+  display: none;
+  @media ${device.md} {
+    display: block;
+  }
+`;
+
+export const Search = styled(SearchAnt)`
+  && {
+    input {
+      background-color: ${(props) => props.theme.formBg};
+      color: ${(props) => props.theme.bodyColor};
+      border: 1px solid ${(props) => props.theme.formBorder};
+    }
+
+    input:hover {
+      border-color: ${(props) => props.theme.formBorderHover};
+    }
+    input:focus {
+      background-color: ${(props) => props.theme.formBgFocus};
+      border-color: ${(props) => props.theme.focus};
+      box-shadow: 0 0 0 1px ${(props) => props.theme.focus};
+    }
+
+    input::placeholder {
+      color: ${(props) => props.theme.formPlaceholderColor};
+    }
+    .ant-input-group-addon {
+      background-color: ${(props) => props.theme.btnBg};
+    }
+
+    button {
+      color: ${(props) => props.theme.btnColor}!important;
+      background-color: ${(props) => props.theme.btnBg}!important;
+      border: 1px solid ${(props) => props.theme.formBorder}!important;
+    }
+
+    button:hover {
+      color: ${(props) => props.theme.btnColor} !important;
+      border-color: ${(props) => props.theme.formBorderHover}!important;
+    }
+  }
+`;
+
+export const RightNavbar = styled.div`
+  @media ${device.md} {
+    flex: auto;
+  }
+`;
+export const MobileBtnWrapper = styled(Button)`
+  background-color: ${(props) => props.theme.btnBg};
+  color: ${(props) => props.theme.btnColor};
+  border-color: ${(props) => props.theme.base[30]};
+  @media ${device.md} {
+    display: none;
+  }
+`;
+export const MenuWrapper = styled(Menu)`
+  && {
+    justify-content: end;
+    border-bottom: none;
+    display: none;
+    background-color: transparent;
+    color: ${(props) => props.theme.menuColor};
+
+    @media ${device.md} {
+      display: flex;
+    }
+
+    .ant-menu-item:hover {
+      color: ${(props) => props.theme.menuColor};
+    }
+
+    .ant-menu-item:hover::after {
+      border-color: ${(props) => props.theme.menuLinkBorderColor};
+    }
+    .ant-menu-item:hover:not(.ant-menu-item-selected):not(
+        .ant-menu-submenu-selected
+      ) {
+      color: ${(props) => props.theme.menuColor};
+    }
+  }
+`;
+
+export const DrawerWrapper = styled(Drawer)`
+  && {
+    background-color: ${(props) => props.theme.drawerBg};
+
+    .ant-drawer-close {
+      color: ${(props) => props.theme.drawerColor};
+    }
+    .ant-drawer-title {
+      color: ${(props) => props.theme.drawerColor};
+    }
+  }
+`;
+export const MobileMenuWrapper = styled(Menu)`
+  && {
+    background-color: transparent;
+    justify-content: end;
+    color: ${(props) => props.theme.menuColor};
+
+    .ant-menu-item:hover:not(.ant-menu-item-selected):not(
+        .ant-menu-submenu-selected
+      ) {
+      color: ${(props) => props.theme.menuColor};
+    }
+
+    .ant-menu-submenu {
+      color: ${(props) => props.theme.menuColor};
+    }
+    .ant-menu.ant-menu-sub {
+      background-color: ${(props) => props.theme.menuBg};
+    }
+  }
+`;
